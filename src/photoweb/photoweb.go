61479af184e7cfc16ae0c42c33972fa2bdbd2eae
zoe <zoe@offerhk.com>
2017-11-09 18:50:17 +0800

    modify

--- src/photoweb/photoweb.go
+++ src/photoweb/photoweb.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+)
+
+const (
+	UPLOAD_DIR = "./uploads"
 )
 
 func uploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,4 +21,68 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 			"</form>")
 		return
 	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		filename := h.Filename
+		defer f.Close()
+		t, err := os.Create(UPLOAD_DIR + "/" + filename)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer t.Close()
+		if _, err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	if exists := isExists(imagePath); !exists {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir(UPLOAD_DIR)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	var listHtml string
+	for _, fileInfo := range fileInfoArr {
+		imgid := fileInfo.Name
+		listHtml += "<li><a href=\"/view?id=" + imgid + "\">" + imgid + "</a></li>"
+	}
+	io.WriteString(w, "<ol>"+listHtml+"</ol>")
+}
+
+func isExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
+}
+
+func main() {
+	http.HandleFunc("/", listHandler)
+	http.HandleFunc("/view", viewHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("listenandserve:", err.Error())
+	}
+
 }
