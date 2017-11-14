e5003f425e583b4db3d4a4e6d07f52680863e231
zoe <zoe@offerhk.com>
2017-11-14 18:31:52 +0800

    modify

---
+++ src/photoweb/photoweb_tmpl.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+const (
+	UPLOAD_DIR = "./uploads"
+)
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		if err := randerHtml(w, "upload", nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		t.Execute(w, nil)
+		return
+	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		filename := h.Filename
+		defer f.Close()
+		t, err := os.Create(UPLOAD_DIR + "/" + filename)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer t.Close()
+		if _, err := io.Copy(t, f); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(w, r, "/view?id="+filename, http.StatusFound)
+	}
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	if exists := isExists(imagePath); !exists {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
+}
+
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir("./uploads")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	locals := make(map[string]interface{})
+	images := []string{}
+	for _, fileInfo := range fileInfoArr {
+		images = append(images, fileInfo.Name())
+	}
+	locals["images"] = images
+	t, err := template.ParseFiles("list.html")
+	if err := renderHtml(w, "list", locals); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	t.Execute(w, locals)
+	return
+}
+
+func renderHtml(w http.ResponseWriter, tmpl string, locals map[string]interface{}) (err error) {
+	t, err = template.ParseFiles(tmpl + ".html")
+	if err != nil {
+		return
+	}
+	err = t.Execute(w, locals)
+	return
+}
+func isExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
+}
+
+func main() {
+	http.HandleFunc("/", listHandler)
+	http.HandleFunc("/view", viewHandler)
+	http.HandleFunc("/upload", uploadHandler)
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		log.Fatal("listenandserve:", err.Error())
+	}
+}
+
+func init() {
+
+}
